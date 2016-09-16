3019c5a936dbf02a34e07fbf9d41f9028f252d83
giovanni <giovanniburbi10@gmail.com>
2016-09-16 16:56:38 +0200

    second commit. aggiunta lib dei test

---
+++ test/DownloaderTest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by giovanni on 16/09/16.
+//
+#include <gtest/gtest.h>
+#include "../Downloader.h"
+
+TEST(addtests,listdownloadfiles){
+    //crea oggetto e fai verifiche
+    //ASSERT_EQ();
+}
+
+TEST(downloadfiles,listdownloadfiles){
+    //fai
+}
+
+
